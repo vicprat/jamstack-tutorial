52d791f8413fa05478239371b5ccaba664d0d16a
Víctor Prado <vicknan0182@gmail.com>
2022-08-01 23:50:50 -0500

    jamstack with graphql and hygraph

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: ["media.graphassets.com", "localhost"],
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
